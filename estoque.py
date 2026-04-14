7a0f14c7bc66852f79748634e1b2fb340bb04cd8
Pedro Monteiro <p10dromonteiro.go@gmail.com>
2026-04-13 23:40:50 -0400

    adiciona função de adicionar produto ao estoque

--- estoque.py
+++ estoque.py
@@ -3,7 +3,25 @@ estoque = {
     "maçã": 5,
     "laranja": 8,
     "cenoura": 4,
+    
 }
 
 for nome, quantidade in estoque.items():
     print(f"{nome}: {quantidade}")
+
+
+def adicionarProduto():
+    novoProduto = input("Adicione um novo porduto: ")
+    quantidadeNovoProduto = int(input("Adicione a quantidade: "))
+
+    estoque[novoProduto] = quantidadeNovoProduto
+
+    for nome, quantidade in estoque.items():
+        print(f"{nome}: {quantidade}")
+
+
+
+
+
+
+adicionarProduto()
